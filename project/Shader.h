eff4f1ab63049b5dffec6b81c5b80005a7c042b3
Kasia <sealionkat@gmail.com>
2016-10-17 17:56:01 +0200

    showing triangle

--- project/Shader.h
+++ project/Shader.h
@@ -10,10 +10,10 @@
 
 class Shader
 {
-public:
+  public:
     GLuint Program;
     // Constructor generates the shader on the fly
-    Shader(const GLchar* vertexPath, const GLchar* fragmentPath, const GLchar* geometryPath = nullptr)
+    Shader(const GLchar *vertexPath, const GLchar *fragmentPath, const GLchar *geometryPath = nullptr)
     {
         // 1. Retrieve the vertex/fragment source code from filePath
         std::string vertexCode;
@@ -23,10 +23,10 @@ public:
         std::ifstream fShaderFile;
         std::ifstream gShaderFile;
         // ensures ifstream objects can throw exceptions:
-        vShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
-        fShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
-        gShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
-        try 
+        vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+        fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+        gShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+        try
         {
             // Open files
             vShaderFile.open(vertexPath);
@@ -34,29 +34,29 @@ public:
             std::stringstream vShaderStream, fShaderStream;
             // Read file's buffer contents into streams
             vShaderStream << vShaderFile.rdbuf();
-            fShaderStream << fShaderFile.rdbuf();		
+            fShaderStream << fShaderFile.rdbuf();
             // close file handlers
             vShaderFile.close();
             fShaderFile.close();
             // Convert stream into string
             vertexCode = vShaderStream.str();
-            fragmentCode = fShaderStream.str();			
-			// If geometry shader path is present, also load a geometry shader
-			if(geometryPath != nullptr)
-			{
+            fragmentCode = fShaderStream.str();
+            // If geometry shader path is present, also load a geometry shader
+            if (geometryPath != nullptr)
+            {
                 gShaderFile.open(geometryPath);
                 std::stringstream gShaderStream;
-				gShaderStream << gShaderFile.rdbuf();
-				gShaderFile.close();
-				geometryCode = gShaderStream.str();
-			}
+                gShaderStream << gShaderFile.rdbuf();
+                gShaderFile.close();
+                geometryCode = gShaderStream.str();
+            }
         }
         catch (std::ifstream::failure e)
         {
             std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
         }
-        const GLchar* vShaderCode = vertexCode.c_str();
-        const GLchar * fShaderCode = fragmentCode.c_str();
+        const GLchar *vShaderCode = vertexCode.c_str();
+        const GLchar *fShaderCode = fragmentCode.c_str();
         // 2. Compile shaders
         GLuint vertex, fragment;
         GLint success;
@@ -70,59 +70,64 @@ public:
         fragment = glCreateShader(GL_FRAGMENT_SHADER);
         glShaderSource(fragment, 1, &fShaderCode, NULL);
         glCompileShader(fragment);
-		checkCompileErrors(fragment, "FRAGMENT");
-		// If geometry shader is given, compile geometry shader
-		GLuint geometry;
-		if(geometryPath != nullptr)
-		{
-			const GLchar * gShaderCode = geometryCode.c_str();
-			geometry = glCreateShader(GL_GEOMETRY_SHADER);
-			glShaderSource(geometry, 1, &gShaderCode, NULL);
-			glCompileShader(geometry);
-			checkCompileErrors(geometry, "GEOMETRY");
-		}
+        checkCompileErrors(fragment, "FRAGMENT");
+        // If geometry shader is given, compile geometry shader
+        GLuint geometry;
+        if (geometryPath != nullptr)
+        {
+            const GLchar *gShaderCode = geometryCode.c_str();
+            geometry = glCreateShader(GL_GEOMETRY_SHADER);
+            glShaderSource(geometry, 1, &gShaderCode, NULL);
+            glCompileShader(geometry);
+            checkCompileErrors(geometry, "GEOMETRY");
+        }
         // Shader Program
         this->Program = glCreateProgram();
         glAttachShader(this->Program, vertex);
         glAttachShader(this->Program, fragment);
-		if(geometryPath != nullptr)
-			glAttachShader(this->Program, geometry);
+        if (geometryPath != nullptr)
+        {
+            glAttachShader(this->Program, geometry);
+        }
         glLinkProgram(this->Program);
         checkCompileErrors(this->Program, "PROGRAM");
         // Delete the shaders as they're linked into our program now and no longer necessery
         glDeleteShader(vertex);
         glDeleteShader(fragment);
-		if(geometryPath != nullptr)
-			glDeleteShader(geometry);
-
+        if (geometryPath != nullptr)
+        {
+            glDeleteShader(geometry);
+        }
     }
     // Uses the current shader
     void Use() { glUseProgram(this->Program); }
 
-private:
+  private:
     void checkCompileErrors(GLuint shader, std::string type)
-	{
-		GLint success;
-		GLchar infoLog[1024];
-		if(type != "PROGRAM")
-		{
-			glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-			if(!success)
-			{
-				glGetShaderInfoLog(shader, 1024, NULL, infoLog);
-                std::cout << "| ERROR::::SHADER-COMPILATION-ERROR of type: " << type << "|\n" << infoLog << "\n| -- --------------------------------------------------- -- |" << std::endl;
-			}
-		}
-		else
-		{
-			glGetProgramiv(shader, GL_LINK_STATUS, &success);
-			if(!success)
-			{
-				glGetProgramInfoLog(shader, 1024, NULL, infoLog);
-                std::cout << "| ERROR::::PROGRAM-LINKING-ERROR of type: " << type << "|\n" << infoLog << "\n| -- --------------------------------------------------- -- |" << std::endl;
-			}
-		}
-	}
+    {
+        GLint success;
+        GLchar infoLog[1024];
+        if (type != "PROGRAM")
+        {
+            glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+            if (!success)
+            {
+                glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+                std::cout << "| ERROR::::SHADER-COMPILATION-ERROR of type: " << type << "|\n"
+                          << infoLog << "\n| -- --------------------------------------------------- -- |" << std::endl;
+            }
+        }
+        else
+        {
+            glGetProgramiv(shader, GL_LINK_STATUS, &success);
+            if (!success)
+            {
+                glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+                std::cout << "| ERROR::::PROGRAM-LINKING-ERROR of type: " << type << "|\n"
+                          << infoLog << "\n| -- --------------------------------------------------- -- |" << std::endl;
+            }
+        }
+    }
 };
 
 #endif
--- project/main.cpp
+++ project/main.cpp
@@ -57,7 +57,7 @@ int main()
     std::cout << "Preparing shaders..." << std::endl;
 
     const GLchar* vertexPath = "shaders/vertexShader.glsl";
-    const GLchar* fragmentPath = "shaders/vertexShader.glsl";
+    const GLchar* fragmentPath = "shaders/fragmentShader.glsl";
 
     Shader shaderMtn(vertexPath, fragmentPath);
 
@@ -73,11 +73,23 @@ int main()
         0.0f, 0.5f, 0.0f
     };
 
-    // VBO
-    GLuint VBO;
+    // VBO, VAO
+    GLuint VBO, VAO;
+    glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    // Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+    glBindVertexArray(VAO);
+
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
+
+    glBindVertexArray(0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs)
+
 
     /* /Objects setup */
 
@@ -94,6 +106,9 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);
 
         shaderMtn.Use();
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
 
         // Swap the screen buffers
         glfwSwapBuffers(window);
