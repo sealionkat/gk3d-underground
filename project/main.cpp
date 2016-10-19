2710fc6de03abdcb8f3bd3ea49354dc7f85c058d
Kasia <sealionkat@gmail.com>
2016-10-19 21:54:31 +0200

    fix radians

--- project/main.cpp
+++ project/main.cpp
@@ -149,7 +149,7 @@ int main()
 
         model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
         view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-        projection = glm::perspective(45.0f, (float)width / (float)height, 0.1f, 100.0f);
+        projection = glm::perspective(glm::radians(45.0f), (float)width / (float)height, 0.1f, 100.0f);
 
         GLint modelLoc = glGetUniformLocation(shaderMtn.Program, "model");
         glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
