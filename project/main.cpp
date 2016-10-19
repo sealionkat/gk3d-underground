87bcc11315e306fd71c41547d4c408b034262f3e
Kasia <sealionkat@gmail.com>
2016-10-19 19:54:59 +0200

    adding GLM library

--- project/main.cpp
+++ project/main.cpp
@@ -7,6 +7,11 @@
 // GLFW
 #include <GLFW/glfw3.h>
 
+// GLM 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include "Shader.h"
 
 // Function prototypes
