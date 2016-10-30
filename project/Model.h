83959a6203d41d5e2e5da4b8fee814a6bd914b74
Katarzyna Fokow <sealionkat@gmail.com>
2016-10-30 15:16:37 +0100

    model class mockup

--- project/Model.h
+++ project/Model.h
@@ -1,8 +1,26 @@
 #ifdef MODEL_H
 #define MODEL_H
 
+#include <GL/glew.h>
+
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 class Model
 {
+  public:
+    glm::vec3 Position;
+    glm::vec3 Color;
+
+    glm::vec3 ambient;
+    glm::vec3 diffuse;
+    glm::vec3 specular;
+    GLfloat shininess;
+
+    GLfloat vertices[];
 }
 
 #endif // !MODEL_H
